ac11a76f0455cc3a5ca591383cc1417bc23f33dc
Rahul Kumar <rahulrahulsinha777@gmail.com>
2025-09-20 04:00:22 +0530

    fix: Restructure repository with separate gitignore files for frontend and backend

--- Frontend/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authReducer from '../authSlice';
-
-export const store = configureStore({
-    reducer:{
-        auth: authReducer  //auth hmare slice ka name hogua or authReducer jo hmare reducer hai
-    }
-}); 
